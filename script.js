87cfc180f7a5eb24d2080439a823720e53af7a5f
Iwan Rahman <1w3n.rahman@gmail.com>
2023-07-22 15:18:01 -0400

    Create Book constructor

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let myLibrary = [];
 
 function Book(title, author, pages, isAvailable){
-
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = isAvailable;
 }
 
 function addBookToLibrary(book){
